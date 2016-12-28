aa23f4821be9a0ed6f7688bf94f78dcd0dfef270
Chislov Oleg <chislov.oleg@yahoo.com>
2016-12-29 04:26:47 +0500

    fastfix

--- roles.sql
+++ roles.sql
@@ -14,7 +14,7 @@ grant all on all tables in schema public to admin;
 grant select on member to user;
 grant select on ride to user;
 grant select on request to user;
-grant select on member_preference;
+grant select on member_preference to user;
 grant execute on function approved_requests(ride_id int) to user
 
 grant select on ride to guest;
